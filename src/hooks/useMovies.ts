1d508af3035012534576eff97a76c926870f2141
Julio Ucharima <julio_ucharima@usmp.pe>
2023-11-25 15:16:35 -0500

    refactor: update hook for response & search param

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,23 +1,54 @@
-import { type Movie } from '../interfaces/Movie';
-import responseMovies from '../mocks/with-result.json';
-// import withoutResult from './mocks/no-result.json';
+import { useState } from 'react';
+import { type ResponseWithData, type Movie } from '../interfaces/Movie';
+import withResult from '../mocks/with-result.json';
+import withoutResult from '../mocks/no-result.json';
 
 interface Props {
-  movies: Movie[]
+  movies: Movie[];
+  getMovies: () => void;
 }
 
-export const useMovies = (): Props => {
-  const movies = responseMovies.Search;
+interface ResponseData {
+  Search: ResponseWithData[];
+}
 
-  const mappedMovie: Movie[] = movies?.map(movie => ({
+export const useMovies = (search: string): Props => {
+  const [responseMovies, setResponseMovies] = useState<ResponseData>({
+    Search: [],
+  });
+
+  const movies: ResponseWithData[] = responseMovies.Search;
+
+  const mappedMovie: Movie[] = movies?.map((movie) => ({
     id: movie.imdbID,
     title: movie.Title,
     year: movie.Year,
     type: movie.Type,
-    poster: movie.Poster
-  }))
+    poster: movie.Poster,
+  }));
+
+  const transformResponse = (apiResponse: any): ResponseData => {
+    if (apiResponse.Response === 'True') {
+      // Si la respuesta es exitosa, devolver la parte de búsqueda
+      return { Search: apiResponse.Search };
+    }
+    // Si hay un error, devolver una estructura con un array vacío
+    return { Search: [] };
+  };
+
+  const getMovies = (): void => {
+    if (search !== '') {
+      const transformedResponse = transformResponse(withResult);
+      setResponseMovies(transformedResponse);
+      return;
+    }
+
+    const transformedResponse = transformResponse(withoutResult);
+    setResponseMovies(transformedResponse);
+  };
 
   return {
-    movies: mappedMovie
-  }
-}
+    movies: mappedMovie,
+    getMovies,
+  };
+};
