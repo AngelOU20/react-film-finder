bb24d1401109dac7e20c128e11b909a4d5320eb8
Julio Ucharima <julio_ucharima@usmp.pe>
2023-11-25 15:20:59 -0500

    refactor: update MovieList component

--- src/components/movies/MovieList.tsx
+++ src/components/movies/MovieList.tsx
@@ -1,26 +1,25 @@
 import PropTypes from 'prop-types';
 import { type Movie } from '../../interfaces/Movie';
+import './Movie.scss';
 
 interface Props {
-  movies?: Movie[]
+  movies?: Movie[];
 }
 
 export const MovieList: React.FC<Props> = ({ movies }) => {
   return (
-    <ul>
-    {
-      movies?.map(movie => (
+    <ul className="movies">
+      {movies?.map((movie) => (
         <li key={movie.id}>
           <h3>{movie.title}</h3>
           <p>{movie.year}</p>
           <img src={movie.poster} alt={movie.title} />
         </li>
-      ))
-    }
-  </ul>
-  )
-}
+      ))}
+    </ul>
+  );
+};
 
 MovieList.propTypes = {
-  movies: PropTypes.array.isRequired
+  movies: PropTypes.array.isRequired,
 };
