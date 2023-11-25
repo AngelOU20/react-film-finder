568e604b290a8c79b8a72895e7540ee198085cd3
Julio Ucharima <julio_ucharima@usmp.pe>
2023-11-25 15:11:26 -0500

    style: format Movie interface and add new types

--- src/interfaces/Movie.ts
+++ src/interfaces/Movie.ts
@@ -1,7 +1,20 @@
 export interface Movie {
-  title: string
-  year: string
-  id: string
-  type: string
-  poster: string
+  title: string;
+  year: string;
+  id: string;
+  type: string;
+  poster: string;
+}
+
+export interface ResponseWithData {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+}
+
+export interface ResponseWithoutData {
+  Response: string;
+  Error: string;
 }
