1a8c42155ad9aa869e515478b7b5ba3e23092eb5
Julio Ucharima <julio_ucharima@usmp.pe>
2023-11-26 14:13:17 -0500

    Refactor: improve logic and movie handling in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,32 @@ import { Header, MovieList, MovieNoResults } from './components';
 import { useSearch, useMovies } from './hooks';
 
 function App(): JSX.Element {
-  const { search } = useSearch();
-  const { movies, getMovies } = useMovies(search);
-  const hasMovies = movies?.length > 0;
+  const { search, setSearch, error } = useSearch();
+  const { movies, loading, getMovies } = useMovies({ search });
+  const hasMovies =
+    movies !== null && movies !== undefined && movies.length > 0;
+
+  const renderComponent = (): JSX.Element => {
+    if (loading) return <p>Loading...</p>;
+
+    if (hasMovies) return <MovieList movies={movies} />;
+
+    return <MovieNoResults />;
+  };
 
   return (
     <div className="page container">
       <h1 className="title">Buscador de películas</h1>
-      <Header getMovies={getMovies} />
+      <Header
+        search={search}
+        setSearch={setSearch}
+        error={error}
+        getMovies={getMovies}
+      />
       <main className="main">
         {/* Aqui iran las peliculas */}
-        {hasMovies ? <MovieList movies={movies} /> : <MovieNoResults />}
+        {renderComponent()}
+        {/* {hasMovies ? <MovieList movies={movies} /> : <MovieNoResults />} */}
       </main>
     </div>
   );
