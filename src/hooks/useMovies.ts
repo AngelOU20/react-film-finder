631554b034c0fbb986a8e9fc1e74b8452629d81d
Julio Ucharima <julio_ucharima@usmp.pe>
2023-11-26 14:54:11 -0500

    feat: add useRef to avoid the same search

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { type Movie } from '../interfaces/Movie';
 import { searchMovies } from '../services/movies';
 
@@ -13,11 +13,16 @@ export const useMovies = ({ search }: { search: string }): Props => {
   const [movies, setMovies] = useState<Movie[] | null>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const previousSearch = useRef(search);
 
   const getMovies = async (): Promise<void> => {
+    if (previousSearch.current === search) return;
+
     try {
       setLoading(true);
       setError(null);
+
+      previousSearch.current = search;
 
       const newMovies = await searchMovies({ search });
       setMovies(newMovies);
