d77d127d07daf4c8a01093e878237e24ca852084
Julio Ucharima <julio_ucharima@usmp.pe>
2023-11-26 15:55:47 -0500

    feat: implement sorting functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { useState, useEffect } from 'react';
 import { Header, MovieList, MovieNoResults } from './components';
 import { useSearch, useMovies } from './hooks';
 
 function App(): JSX.Element {
+  const [sort, setSort] = useState(false);
+
   const { search, setSearch, error } = useSearch();
-  const { movies, loading, getMovies } = useMovies({ search });
+  const { movies, loading, getMovies } = useMovies({ search, sort });
   const hasMovies =
     movies !== null && movies !== undefined && movies.length > 0;
 
@@ -15,6 +18,14 @@ function App(): JSX.Element {
     return <MovieNoResults />;
   };
 
+  useEffect(() => {
+    console.log('renderizado del getMovies');
+  }, [getMovies]);
+
+  const handleSort = (): void => {
+    setSort(!sort);
+  };
+
   return (
     <div className="page container">
       <h1 className="title">Buscador de películas</h1>
@@ -23,6 +34,8 @@ function App(): JSX.Element {
         setSearch={setSearch}
         error={error}
         getMovies={getMovies}
+        sort={sort}
+        handleSort={handleSort}
       />
       <main className="main">
         {/* Aqui iran las peliculas */}
