6e6abce8231353e566e436a69b47ddbcdb84dfcc
Julio Ucharima <julio_ucharima@usmp.pe>
2023-11-26 18:52:53 -0500

    refactor: Enhance presentation in MovieList

--- src/components/movies/MovieList.tsx
+++ src/components/movies/MovieList.tsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { type Movie } from '../../interfaces/Movie';
+import { FaPlay } from 'react-icons/fa';
 import './Movie.scss';
 
 interface Props {
@@ -10,10 +11,13 @@ export const MovieList: React.FC<Props> = ({ movies }) => {
   return (
     <ul className="movies">
       {movies?.map((movie) => (
-        <li key={movie.id}>
-          <h3>{movie.title}</h3>
-          <p>{movie.year}</p>
-          <img src={movie.poster} alt={movie.title} />
+        <li className="card movie_card" key={movie.id}>
+          <img src={movie.poster} alt={movie.title} className="card-img-top" />
+          <div className="card-body">
+            <FaPlay className="play_button" />
+            <h3 className="card-title">{movie.title}</h3>
+            <span className="movie_info">{movie.year}</span>
+          </div>
         </li>
       ))}
     </ul>
