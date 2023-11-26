5ea5fc7e20e81feff36df2e8d8a2dd6bbf8044b0
Julio Ucharima <julio_ucharima@usmp.pe>
2023-11-26 14:18:26 -0500

    feat: add movie search service for data fetching

---
+++ src/services/movies.ts
@@ -0,0 +1,30 @@
+import { type Movie, type ResponseWithData } from '../interfaces/Movie';
+
+const API_KEY = 'ee6d55fa';
+
+export const searchMovies = async ({
+  search,
+}: {
+  search: string;
+}): Promise<Movie[] | null> => {
+  if (search === '') return null;
+
+  try {
+    const resp = await fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`
+    );
+    const data = await resp.json();
+
+    // Note: Mappeo de datos
+    const movies: ResponseWithData[] = data.Search;
+    return movies?.map((movie) => ({
+      id: movie.imdbID,
+      title: movie.Title,
+      year: movie.Year,
+      type: movie.Type,
+      poster: movie.Poster,
+    }));
+  } catch (error) {
+    throw new Error('Error searching movies');
+  }
+};
